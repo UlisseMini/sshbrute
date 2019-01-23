df578cf949182454b5bc8c96273b50293dd1d733
UlisseMini <ulisse.mini@gmail.com>
2019-01-23 10:17:04 -0500

    Added option for the number of concurrent requests

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 // TODO
 // Instead of options for username etc have it in args in the form user@adress:port
 // If ssh password auth is not supported, detect it and stop the program
-// Set number of goroutines
 
 // Package main implements a simple ssh bruteforce tool to use with wordlists.
 package main
@@ -28,7 +27,9 @@ var (
 	timeout = flag.Duration("t", 300*time.Millisecond,
 		"Set the timeout depending on the latency between you and the remote host.")
 
-	debug = flag.Bool("d", false, "debug mode, print logs to stderr")
+	debug   = flag.Bool("d", false, "debug mode, print logs to stderr")
+	workers = flag.Int("g", 32,
+		"how meny goroutines should be making concurrent connections")
 )
 
 func main() {
@@ -71,19 +72,29 @@ func main() {
 		log.Println("done recv from finished")
 	}()
 
+	lines := make(chan string)
 	var wg sync.WaitGroup
+	// create goroutine workers
+	for i := 0; i < *workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for line := range lines {
+				log.Printf("create task for %q", line)
+				t := fac.make(line)
+
+				t.do()
+				finished <- t
+			}
+		}()
+	}
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		log.Printf("create task for %q", line)
-		t := fac.make(line)
-
-		wg.Add(1)
-		go func(t task) {
-			defer wg.Done()
-			t.do()
-			finished <- t
-		}(t)
+		log.Printf("sending %q over lines (chan string)", line)
+		lines <- line
 	}
+	close(lines)
 
 	// wait for the tasks to be done
 	log.Println("waiting for WaitGroup")
